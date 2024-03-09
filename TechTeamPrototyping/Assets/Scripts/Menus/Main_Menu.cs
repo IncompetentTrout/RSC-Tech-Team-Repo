f77b5339e0abefbec22f2a5064e4ac5aac130589
IncompetentTrout <tlam2020@elam.co.uk>
2024-03-09 23:13:48 +0000

    Minor Pause Menu Fixes

---
+++ TechTeamPrototyping/Assets/Scripts/Menus/Main_Menu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject Title_Screen;
+
+    
+    void Start()
+    {
+        TitleScreenButton();
+    }
+
+   
+    public void StartButton()
+    {
+        // Play Now Button has been pressed loads first level
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
+    }
+
+    public void TitleScreenButton()
+    {
+        // Shows title screen
+        mainMenu.SetActive(false);
+        Title_Screen.SetActive(true);
+    }
+
+    
+
+    public void title_Screen_Play_Button()
+    {
+        // Show Main Menu
+        mainMenu.SetActive(true);
+        Title_Screen.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+        
+    }
+}
